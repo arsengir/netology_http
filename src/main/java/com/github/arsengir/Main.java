f82aae93b545383baed2c4a4fb475a46357d89a7
Arsen Girinov <amateurlib@yandex.ru>
2021-03-28 10:09:50 +0300

    task2

--- src/main/java/com/github/arsengir/Main.java
+++ src/main/java/com/github/arsengir/Main.java
@@ -1,6 +1,5 @@
 package com.github.arsengir;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.apache.http.client.config.RequestConfig;
@@ -10,13 +9,15 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.List;
 
 public class Main
 {
+    public static final String URI = "https://api.nasa.gov/planetary/apod?api_key=SF8ar4Yt40TD6cAZqC5axwz6LfUXkEaRIbeVMGLr";
 
-    public static final String URI = "https://cat-fact.herokuapp.com/facts";
     public static final ObjectMapper mapper = new ObjectMapper()
             .registerModules(new JavaTimeModule(), new ParameterNamesModule())
             .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
@@ -32,17 +33,21 @@ public class Main
                 .build();
 
         HttpGet request = new HttpGet(URI);
-
         CloseableHttpResponse response = httpClient.execute(request);
+        NasaFotoInfo nasaFotoInfo = mapper.readValue(response.getEntity().getContent(), NasaFotoInfo.class);
 
-        List<CatFact> catFacts = mapper.readValue(response.getEntity().getContent(),
-                new TypeReference<List<CatFact>>() {});
+        String urlFoto = nasaFotoInfo.getUrl();
+        String fileName = urlFoto.substring(urlFoto.lastIndexOf('/')+1);
 
-        // т.к. сайт больше не возвращает поле Upvotes, чтобы продемонстрировать работу с filter просто отфилтровал
-        // один факт по _id
-        catFacts.stream()
-                .filter(e -> !e.get_id().equals("58e008800aac31001185ed07"))
-                .forEach(System.out::println);
+        request = new HttpGet(urlFoto);
+        response = httpClient.execute(request);
 
+        try(BufferedOutputStream bout = new BufferedOutputStream(new FileOutputStream(fileName))) {
+            byte[] data = new byte[1024];
+            int count;
+            while ((count = response.getEntity().getContent().read(data, 0, 1024)) != -1) {
+                bout.write(data, 0, count);
+            }
+        }
     }
 }
